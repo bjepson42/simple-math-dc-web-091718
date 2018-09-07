0e2b148989dad39c9969504280af61b3aa660998
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 18:26:51 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,11 +19,17 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
+  num3 = num1 % num2
+  num3
 end
 
 def square_root(num)
+  num3 = Math.squrt(num)
+  num3
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  num5 = num1 + ((num2*num3)/num4)
+  num5
 end
