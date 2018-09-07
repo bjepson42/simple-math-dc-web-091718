74758adc16f45e7718e1e0e32b75ef9842adbcb2
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 18:24:21 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,9 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  num3 = num1 / num2
+  num3
 end
 
 def multiplication(num1, num2)
+  num3 = num1 * num2
+  num3
 end
 
 def modulo(num1, num2)
